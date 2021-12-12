19462615e548b6f8f59d4d353c4261ad3829f4d3
Cuderbot <ja.becerraa@gmail.com>
2021-12-12 09:22:52 -0300

    feat: add new layers (VAL,GEN)

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -23,6 +23,8 @@ enum planck_layers {
   _NUMBER,
   _NAVIGATION,
   _ADJUST,
+  _GAM_VAL,
+  _GAM_GEN,
 };
 
 enum planck_keycodes {
@@ -31,58 +33,103 @@ enum planck_keycodes {
   // EXT_PLV
 };
 
-#define NUMBER MO(_NUMBER)
-#define NAVIGATION MO(_NAVIGATION)
+// Layers shortcut
+#define NUM MO(_NUMBER)
+#define NAV MO(_NAVIGATION)
+#define VAL TG(_GAM_VAL)
+#define GEN TG(_GAM_GEN)
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Qwerty */
+/* QWERTY
+ * ,-----------------------------------------------------------------------------------.
+ * | CTAB |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | AESC |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |SFT/( |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |NUM   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      | GUI  |     SPC     |     ENT     | GUI  |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
 [_QWERTY] = LAYOUT_planck_grid(
-  /* ,---------------------------------------------------------------------------------------------------------------------------------------. */
-        LALT_T(KC_TAB), KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,       KC_BSPC,
-  /* |---------------------------------------------+---------------------------------------+-------------------------------------------------|*/
-        CTL_T(KC_ESC),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,    KC_ENT,
-  /* |---------------------------------------------+---------------------------------------+-------------------------------------------------|*/
-        KC_LSFT,        KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_UP,      LSFT_T(KC_SLSH),
-  /* |---------------------------------------------+---------------------------------------+-------------------------------------------------|*/
-        LOWER,          XXXXXXX,  XXXXXXX,  KC_LGUI,  KC_SPC,   KC_SPC,   RAISE,    KC_ENT,   LALT_T(KC_QUOT),  KC_LEFT,  KC_DOWN,    KC_RGHT
-  /* `---------------------------------------------------------------------------------------------------------------------------------------' */
+    LCTL_T(KC_TAB), _________________QWERTY_L1_________________, _________________QWERTY_R1_________________, KC_DEL,
+    LALT_T(KC_ESC), _________________QWERTY_L2_________________, _________________QWERTY_R2_________________, KC_BSLS,
+    KC_LSPO,        _________________QWERTY_L3_________________, _________________QWERTY_R3_________________, NUM,
+    XXXXXXX,        XXXXXXX, XXXXXXX, KC_LGUI, KC_BSPC, KC_BSPC, KC_ENT, KC_ENT, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,
 ),
 
-/* Raise
+/* Numbers and Symbols
  * ,-----------------------------------------------------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+ * | CTAB |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * | AESC |   `  |   -  |   =  |   [  |   ]  |   \  |      |      |  Up  |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |   -  |   =  |   [  |   ]  |  \   |ISO # |ISO / |Pg Up |Pg Dn |      | Del  |
+ * | LSFT |      |      |      |      |      |      |      | Left | Down |Right | RSFT |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |             |      |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      | GUI  |     SPC     |     ENT     | NEXT |VOLDWN|VOLUP | PLAY |
  * `-----------------------------------------------------------------------------------'
  */
 [_NUMBER] = LAYOUT_planck_grid(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LSFT,
-    _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______, KC_DEL,
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, 
-    _______, _______, _______, KC_LGUI, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    _______,  ________________NUMBER_LEFT________________,  ________________NUMBER_RIGHT________________,  XXXXXXX,
+    _______,  KC_GRV, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC,    KC_NUBS, XXXXXXX, XXXXXXX,  KC_UP,   XXXXXXX,  XXXXXXX, 
+    _______,  ___________________BLANK___________________,  XXXXXXX, XXXXXXX, KC_LEFT,  KC_DOWN, KC_RIGHT, KC_RSFT, 
+    XXXXXXX,  VAL, GEN, _______, _______, _______,          _______, _______, KC_MNXT,  KC_VOLD, KC_VOLU,  KC_MPLY
 ),
 
-/* Lower
+/* Navigation
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
+ * | CTAB |      |      |      |      |      |      |      |      |      |      |QWERTY|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
+ * | AESC |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
+ * | LSFT |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      | GUI  |     SPC     |     ENT     |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_NAVIGATION] = LAYOUT_planck_grid(
-    KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,     KC_UP,     KC_F22, KC_F23, KC_F24,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LEFT,    KC_DOWN,    KC_RIGHT, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, KC_LGUI, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  TG(_QWERTY),
+    _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+),
+
+/* Valorant
+ * ,-----------------------------------------------------------------------------------.
+ * |      |   1  |   2  |   3  |   4  |      |      |      |      |      |      |QWERTY|
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |  TAB |   Q  |   W  |   E  |   R  |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | CTRL |   A  |   S  |   D  |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      | GUI  |     SPC     |     ENT     |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_GAM_VAL] = LAYOUT_planck_grid(
+    XXXXXXX,  KC_1,     KC_2,     KC_3,     KC_4,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  TG(_QWERTY),,
+    KC_TAB,   KC_Q,     KC_W,     KC_E,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    KC_LCTRL, KC_A,     KC_S,     KC_D,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+),
+
+/* Genshin Impact
+ * ,-----------------------------------------------------------------------------------.
+ * |      |   1  |   2  |   3  |   4  |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |  TAB |   Q  |   W  |   E  |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | LSFT |   A  |   S  |   D  |   F  |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      | GUI  |     SPC     |     ENT     |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_GAM_GEN] = LAYOUT_planck_grid(
+    XXXXXXX,  KC_1,     KC_2,     KC_3,     KC_4,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    KC_TAB,   KC_Q,     KC_W,     KC_E,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    KC_LSFT,  KC_A,     KC_S,     KC_D,     KC_F,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
 ),
 
 /* Adjust (Lower + Raise)
@@ -94,7 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|TermOn|TermOf|      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
+ * |      |      |      |      |     SPC     |     ENT     |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
