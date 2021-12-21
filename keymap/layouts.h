c0be6f9dddf86b77765f5df9c3ad33535181e29a
Cuderbot <ja.becerraa@gmail.com>
2021-12-21 13:24:26 -0300

    fix: remove commas from macro lines

--- keymap/layouts.h
+++ keymap/layouts.h
@@ -44,9 +44,9 @@ enum planck_keycodes {
  *                                      ___QWERTY_L3___
  *                                      ___QWERTY_L4___
  */
-#define QWERTY_L1   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T, KC_CTTB, KC_BSPC, KC_Y, KC_U,   KC_I,     KC_O,   KC_P,
-#define QWERTY_L2   KC_A,     KC_S,     KC_D,     KC_F,     KC_G, KC_ALEC, KC_QUOT, KC_H, KC_J,   KC_K,     KC_L,   KC_SCLN,
-#define QWERTY_L3   KC_LSPO,  KC_Z,     KC_X,     KC_C,     KC_V, KC_B,    KC_RSPC, KC_N, KC_M,   KC_COMM,  KC_DOT, KC_SLASH,
+#define QWERTY_L1   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T, KC_CTTB, KC_BSPC, KC_Y, KC_U,   KC_I,     KC_O,   KC_P
+#define QWERTY_L2   KC_A,     KC_S,     KC_D,     KC_F,     KC_G, KC_ALEC, KC_QUOT, KC_H, KC_J,   KC_K,     KC_L,   KC_SCLN
+#define QWERTY_L3   KC_LSPO,  KC_Z,     KC_X,     KC_C,     KC_V, KC_B,    KC_RSPC, KC_N, KC_M,   KC_COMM,  KC_DOT, KC_SLASH
 #define QWERTY_L4   NUM,      _______,  _______,  KC_LGUI,        KC_SPC,         KC_ENT, KC_GRV, KC_MINS,  KC_EQL, KC_NUBS 
 
 /* Numbers and Symbols  (35 keys)
@@ -64,9 +64,9 @@ enum planck_keycodes {
  *                                      ___NUMBER_L3___
  *                                      ___NUMBER_L4___
  */
-#define NUMBER_L1 KC_CTTB,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,
-#define NUMBER_L2 KC_ALEC,  KC_GRV,   KC_MINS,  KC_EQL,   KC_NUBS,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-#define NUMBER_L3 KC_LSFT,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+#define NUMBER_L1 KC_CTTB,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______
+#define NUMBER_L2 KC_ALEC,  KC_GRV,   KC_MINS,  KC_EQL,   KC_NUBS,  _______,  _______,  _______,  _______,  _______,  _______,  _______
+#define NUMBER_L3 KC_LSFT,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
 #define NUMBER_L4 NUM,      _______,  _______,  KC_LGUI,             KC_SPC,             KC_ENT,  _______,  _______,  _______,  _______ 
 
 
@@ -85,9 +85,9 @@ enum planck_keycodes {
  *                                      ___NAVIGA_L3___
  *                                      ___NAVIGA_L4___
  */
-#define NAVIGA_L1 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-#define NAVIGA_L2 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-#define NAVIGA_L3 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+#define NAVIGA_L1 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
+#define NAVIGA_L2 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
+#define NAVIGA_L3 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
 #define NAVIGA_L4 _______,  _______,  _______,  KC_LGUI,             KC_SPC,             KC_ENT,  _______,  _______,  _______,  _______ 
 
 
@@ -108,9 +108,9 @@ enum planck_keycodes {
  *                                      ___ADJUST_L4___
  */
 
-#define ADJUST_L1 RESET,    _______,  DEBUG,    RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  KC_DEL,
-#define ADJUST_L2 _______,  _______,  MU_MOD,   AU_ON,    AU_OFF,   AG_NORM,  AG_SWAP,  QWERTY,   _______,  _______,  _______,  _______,
-#define ADJUST_L3 _______,  MUV_DE,   MUV_IN,   MU_ON,    MU_OFF,   MI_ON,    MI_OFF,   TERM_ON,  TERM_OFF, _______,  _______,  _______,
+#define ADJUST_L1 RESET,    _______,  DEBUG,    RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  KC_DEL
+#define ADJUST_L2 _______,  _______,  MU_MOD,   AU_ON,    AU_OFF,   AG_NORM,  AG_SWAP,  QWERTY,   _______,  _______,  _______,  _______
+#define ADJUST_L3 _______,  MUV_DE,   MUV_IN,   MU_ON,    MU_OFF,   MI_ON,    MI_OFF,   TERM_ON,  TERM_OFF, _______,  _______,  _______
 #define ADJUST_L4 _______,  _______,  _______,  KC_LGUI,             KC_SPC,             KC_ENT,  _______,  _______,  _______,  _______ 
 
 
@@ -129,9 +129,9 @@ enum planck_keycodes {
  *                                      ___VALORA_L3___
  *                                      ___VALORA_L4___
  */
-#define VALORA_L1 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-#define VALORA_L2 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-#define VALORA_L3 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+#define VALORA_L1 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
+#define VALORA_L2 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
+#define VALORA_L3 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
 #define VALORA_L4 _______,  _______,  _______,  KC_LGUI,             KC_SPC,             KC_ENT,  _______,  _______,  _______,  _______
 
 
@@ -150,8 +150,8 @@ enum planck_keycodes {
  *                                      ___GENIMP_L3___
  *                                      ___GENIMP_L4___
  */
-#define GENIMP_L1 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-#define GENIMP_L2 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-#define GENIMP_L3 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+#define GENIMP_L1 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
+#define GENIMP_L2 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
+#define GENIMP_L3 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
 #define GENIMP_L4 _______,  _______,  _______,  KC_LGUI,             KC_SPC,             KC_ENT,  _______,  _______,  _______,  _______
 
