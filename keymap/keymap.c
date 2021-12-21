5e20d9fc1452e14f7d7fa323f8038a757eaf103e
Cuderbot <ja.becerraa@gmail.com>
2021-12-20 21:40:15 -0300

    reimplenting layouts

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -1,45 +1,21 @@
-/* Copyright 2015-2021 Jack Humbert
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include QMK_KEYBOARD_H
 #include "muse.h"
 #include "layouts.h"
 
-enum planck_keycodes {
-  QWERTY = SAFE_RANGE,
-  BACKLIT,
-  EXT_PLV
-};
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_QWERTY]     = LAYOUT_planck_grid(____QWERTY_L1____,  ____QWERTY_R1____,____QWERTY_L2____,  ____QWERTY_R2____,____QWERTY_L3____,  ____QWERTY_R3____,____QWERTY_L4____,  ____QWERTY_R4____,),
-[_NUMBER]     = LAYOUT_planck_grid(____NUMBER_L1____,  ____NUMBER_R1____,____NUMBER_L2____,  ____NUMBER_R2____,____NUMBER_L3____,  ____NUMBER_R3____,____NUMBER_L4____,  ____NUMBER_R4____,),
-[_NAVIGATION] = LAYOUT_planck_grid(____NAVIGA_L1____,  ____NAVIGA_R1____,____NAVIGA_L2____,  ____NAVIGA_R2____,____NAVIGA_L3____,  ____NAVIGA_R1____,____NAVIGA_L4____,  ____NAVIGA_R1____,),
-[_ADJUST]     = LAYOUT_planck_grid(____ADJUST_L1____,  ____ADJUST_R1____,____ADJUST_L2____,  ____ADJUST_R2____,____ADJUST_L3____,  ____ADJUST_R3____,____ADJUST_L4____,  ____ADJUST_R4____,)
+[_QWERTY] = CURRENT_LAYOUT(QWERTY_L1,QWERTY_L2,QWERTY_L3,QWERTY_L4),
+[_NUMBER] = CURRENT_LAYOUT(NUMBER_L1,NUMBER_L2,NUMBER_L3,NUMBER_L4),
+[_NAVIGA] = CURRENT_LAYOUT(NAVIGA_L1,NAVIGA_L2,NAVIGA_L3,NAVIGA_L4),
+[_ADJUST] = CURRENT_LAYOUT(ADJUST_L1,ADJUST_L2,ADJUST_L3,ADJUST_L4),
+[_VALORA] = CURRENT_LAYOUT(VALORA_L1,VALORA_L2,VALORA_L3,VALORA_L4),
+[_GENIMP] = CURRENT_LAYOUT(GENIMP_L1,GENIMP_L2,GENIMP_L3,GENIMP_L4)
 };
 
-#ifdef AUDIO_ENABLE
-  float plover_song[][2]     = SONG(PLOVER_SOUND);
-  float plover_gb_song[][2]  = SONG(PLOVER_GOODBYE_SOUND);
-#endif
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _NUMBER, _NAVIGATION, _ADJUST);
-}
+// layer_state_t layer_state_set_user(layer_state_t state) {
+//   return update_tri_layer_state(state, _NUMBER, _NAVIGATION, _ADJUST);
+// }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
@@ -86,37 +62,37 @@ uint16_t muse_counter = 0;
 uint8_t muse_offset = 70;
 uint16_t muse_tempo = 50;
 
-bool dip_switch_update_user(uint8_t index, bool active) {
-    switch (index) {
-        case 0: {
-#ifdef AUDIO_ENABLE
-            static bool play_sound = false;
-#endif
-            if (active) {
-#ifdef AUDIO_ENABLE
-                if (play_sound) { PLAY_SONG(plover_song); }
-#endif
-                layer_on(_ADJUST);
-            } else {
-#ifdef AUDIO_ENABLE
-                if (play_sound) { PLAY_SONG(plover_gb_song); }
-#endif
-                layer_off(_ADJUST);
-            }
-#ifdef AUDIO_ENABLE
-            play_sound = true;
-#endif
-            break;
-        }
-        case 1:
-            if (active) {
-                muse_mode = true;
-            } else {
-                muse_mode = false;
-            }
-    }
-    return true;
-}
+// bool dip_switch_update_user(uint8_t index, bool active) {
+//     switch (index) {
+//         case 0: {
+// #ifdef AUDIO_ENABLE
+//             static bool play_sound = false;
+// #endif
+//             if (active) {
+// #ifdef AUDIO_ENABLE
+//                 if (play_sound) { PLAY_SONG(plover_song); }
+// #endif
+//                 layer_on(_ADJUST);
+//             } else {
+// #ifdef AUDIO_ENABLE
+//                 if (play_sound) { PLAY_SONG(plover_gb_song); }
+// #endif
+//                 layer_off(_ADJUST);
+//             }
+// #ifdef AUDIO_ENABLE
+//             play_sound = true;
+// #endif
+//             break;
+//         }
+//         case 1:
+//             if (active) {
+//                 muse_mode = true;
+//             } else {
+//                 muse_mode = false;
+//             }
+//     }
+//     return true;
+// }
 
 void matrix_scan_user(void) {
 #ifdef AUDIO_ENABLE
@@ -140,11 +116,13 @@ void matrix_scan_user(void) {
 }
 
 bool music_mask_user(uint16_t keycode) {
-  switch (keycode) {
-    case NUM:
-    case NAV:
-      return false;
-    default:
-      return true;
-  }
+  // switch (keycode) {
+  //   case NUM:
+  //   case NAV:
+  //     return false;
+  //   default:
+  //     return true;
+  // }
+
+  return true;
 }
