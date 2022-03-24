47371ff111bdb8bd25384594a6a205cce69687bb
Cuderbot <ja.becerraa@gmail.com>
2022-03-24 19:52:35 -0300

    feat: update planck config

--- keymap/layouts.h
+++ keymap/layouts.h
@@ -9,7 +9,6 @@ enum planck_layers {
   _NAVIGA,
   _ADJUST,
   _VALORA,
-  _GENIMP,
 };
 
 enum planck_keycodes {
@@ -23,12 +22,11 @@ enum planck_keycodes {
 #define NUM MO(_NUMBER)
 #define NAV MO(_NAVIGA)
 #define VAL TG(_VALORA)
-#define GEN TG(_GENIMP)
 
 // Shortcuts
 #define KC_CTTB LCTL_T(KC_TAB)  // left ctrl(held) -> tab(tap)
 #define KC_ALEC LALT_T(KC_ESC)  // left alt(held) -> esc(tap)
-#define KC_SPGU LGUI_T(KC_SPC)  // left gui(held) -> space(tap)
+#define KC_ENGU LGUI_T(KC_ENT)  // left gui(held) -> enter(tap)
 
 /* QWERTY (35 keys)
  * ,-----------------------------------------------------------------------------------.
@@ -36,39 +34,31 @@ enum planck_keycodes {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | AESC |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | SFT/(|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |NUM   |
+ * | SFT/(|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |)/SFT |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | NUM  |      |  OPT | GUI  |     SPC     |     ENT     |   `  |   -  |   =  |   \  |
+ * | NUM  | CTRL |  OPT | GUI  |     SPC     |     ENT     |   `  |   -  |   =  |   \  |
  * `-----------------------------------------------------------------------------------'
- *                                      ___QWERTY_L1___
- *                                      ___QWERTY_L2___
- *                                      ___QWERTY_L3___
- *                                      ___QWERTY_L4___
  */
-#define QWERTY_L1   KC_CTTB,  KC_Q,     KC_W,     KC_E,     KC_R, KC_T,    KC_Y,    KC_U,   KC_I,     KC_O,     KC_P,     KC_BSPC
-#define QWERTY_L2   KC_ALEC,  KC_A,     KC_S,     KC_D,     KC_F, KC_G,    KC_H,    KC_J,   KC_K,     KC_L,     KC_SCLN,  KC_QUOT
-#define QWERTY_L3   KC_LSPO,  KC_Z,     KC_X,     KC_C,     KC_V, KC_B,    KC_N,    KC_M,   KC_COMM,  KC_DOT,   KC_SLASH, KC_RSPC
-#define QWERTY_L4   NUM,      _______,  KC_LOPT,  KC_LGUI,        KC_SPGU,           KC_ENT, KC_GRV,   KC_MINS,  KC_EQL,   KC_NUBS 
+#define QWERTY_L1   KC_CTTB,  KC_Q,     KC_W,     KC_E,     KC_R, KC_T,    KC_Y,    KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC
+#define QWERTY_L2   KC_ALEC,  KC_A,     KC_S,     KC_D,     KC_F, KC_G,    KC_H,    KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT
+#define QWERTY_L3   KC_LSPO,  KC_Z,     KC_X,     KC_C,     KC_V, KC_B,    KC_N,    KC_M,     KC_COMM,  KC_DOT,   KC_SLASH, KC_RSPC
+#define QWERTY_L4   NUM,      KC_LCTL,  KC_LOPT,  KC_LGUI,        KC_SPC,         KC_ENGU,    KC_GRV,   KC_MINS,  KC_EQL,   KC_BSLS 
 
 /* Numbers and Symbols  (35 keys)
  * ,-----------------------------------------------------------------------------------.
  * | CTTB |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | ALEC |   `  |   -  |   =  |   [  |   ]  |   \  |  Up  |      |      |      |      |
+ * | ALEC |   `  |   -  |   =  |   [  |   ]  |   \  | Left | Down |  Up  |Right |      |      
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | LSFT |      |      |      |      |      | Left | Down |Right | RSFT |      |      |
+ * | LSFT |      |      |      |      |      |      |      |      | RSFT |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |QWERTY|      |      | GUI  |     SPC     |     ENT     | NEXT |VOLDWN|VOLUP | PLAY |
  * `-----------------------------------------------------------------------------------'
- *                                      ___NUMBER_L1___
- *                                      ___NUMBER_L2___
- *                                      ___NUMBER_L3___
- *                                      ___NUMBER_L4___
  */
 #define NUMBER_L1 KC_CTTB,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______
-#define NUMBER_L2 KC_ALEC,  KC_GRV,   KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_NUBS,  KC_UP,    _______,  _______,  _______,  _______
-#define NUMBER_L3 KC_LSFT,  _______,  _______,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,  _______
-#define NUMBER_L4 QWERTY,   _______,  _______,  KC_LGUI,            KC_SPGU,            KC_ENT,   _______,  _______,  _______,  _______ 
+#define NUMBER_L2 KC_ALEC,  KC_GRV,   KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_NUBS,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______
+#define NUMBER_L3 KC_LSFT,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
+#define NUMBER_L4 QWERTY,   _______,  _______,  KC_LGUI,              KC_SPC,           KC_ENGU,  _______,  _______,  _______,  _______ 
 
 
 /* Navigation  (35 keys)
@@ -81,10 +71,6 @@ enum planck_keycodes {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      | GUI  |     SPC     |     ENT     |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
- *                                      ___NAVIGA_L1___
- *                                      ___NAVIGA_L2___
- *                                      ___NAVIGA_L3___
- *                                      ___NAVIGA_L4___
  */
 #define NAVIGA_L1 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
 #define NAVIGA_L2 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
@@ -103,16 +89,12 @@ enum planck_keycodes {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |     SPC     |     ENT     |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
- *                                      ___ADJUST_L1___
- *                                      ___ADJUST_L2___
- *                                      ___ADJUST_L3___
- *                                      ___ADJUST_L4___
  */
 
 #define ADJUST_L1 RESET,    _______,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  KC_DEL,   _______
 #define ADJUST_L2 DEBUG,    _______,  MU_MOD,   AU_ON,    AU_OFF,   AG_NORM,  AG_SWAP,  QWERTY,   _______,  _______,  _______,  _______
 #define ADJUST_L3 _______,  MUV_DE,   MUV_IN,   MU_ON,    MU_OFF,   MI_ON,    MI_OFF,   TERM_ON,  TERM_OFF, _______,  _______,  _______
-#define ADJUST_L4 _______,  _______,  _______,  KC_LGUI,             KC_SPC,             KC_ENT,  _______,  _______,  _______,  _______ 
+#define ADJUST_L4 _______,  _______,  _______,  KC_LGUI,              KC_SPC,           KC_ENGU,  _______,  _______,  _______,  _______ 
 
 
 /* Valorant  (35 keys)
@@ -125,34 +107,8 @@ enum planck_keycodes {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      | GUI  |     SPC     |     ENT     |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
- *                                      ___VALORA_L1___
- *                                      ___VALORA_L2___
- *                                      ___VALORA_L3___
- *                                      ___VALORA_L4___
  */
 #define VALORA_L1 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
 #define VALORA_L2 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
 #define VALORA_L3 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
 #define VALORA_L4 _______,  _______,  _______,  KC_LGUI,             KC_SPC,             KC_ENT,  _______,  _______,  _______,  _______
-
-
-/* Genshin Impact  (35 keys)
- * ,-----------------------------------------------------------------------------------.
- * |      |   1  |   2  |   3  |   4  |      |      |      |      |      |      |QWERTY|
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | TAB  |   Q  |   W  |   E  |   R  |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | CTRL |   A  |   S  |   D  |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      | GUI  |     SPC     |     ENT     |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- *                                      ___GENIMP_L1___
- *                                      ___GENIMP_L2___
- *                                      ___GENIMP_L3___
- *                                      ___GENIMP_L4___
- */
-#define GENIMP_L1 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
-#define GENIMP_L2 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
-#define GENIMP_L3 _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
-#define GENIMP_L4 _______,  _______,  _______,  KC_LGUI,             KC_SPC,             KC_ENT,  _______,  _______,  _______,  _______
-
